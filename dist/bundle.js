a6608617f50caa061588c431cad0951667fefb78
swatawata <sho4725sw@gmail.com>
2025-10-18 01:03:49 +0900

    feat: build

---
+++ dist/bundle.js
@@ -0,0 +1,14 @@
+(() => {
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // src/index.tsx
+  var require_index = __commonJS({
+    "src/index.tsx"() {
+      console.log("log this");
+    }
+  });
+  require_index();
+})();
